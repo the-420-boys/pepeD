811b42f68fa7cd07ed0b5894f4e68d3d6d0093ab
SnoopTheDog <aleksander.vehar@gmail.com>
2020-09-18 14:31:14 +0200

    Update

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    void Update()
+    {
+        
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed; //Value beetween -1 and 1
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //Move character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
